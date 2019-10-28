4916dd5ea971a63710ddfb61b08b544f5f8774d3
utsab <utsabb6@gmail.com>
2019-10-28 17:42:14 +0530

    added temperature modified

--- public/js/test.js
+++ public/js/test.js
@@ -28,6 +28,7 @@ const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const msg1 = document.querySelector("#msg1");
 const msg2 = document.querySelector("#msg2");
+const msg3 = document.querySelector("#msg3");
 
 weatherForm.addEventListener("submit", e => {
   e.preventDefault();
